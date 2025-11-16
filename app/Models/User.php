38c580077b3543c22c8ca6874db95fa4afb69f04
Abiodun Adeosun <hadexemmanuela@gmail.com>
2025-11-16 12:41:00 +0100

    fix(user): HasUuid Trait Add: model/concerns/traits hasuuid,hasformattedname

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,16 +2,16 @@
 
 namespace App\Models;
 
+use App\Models\Concerns\Traits\HasUuid;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable, SoftDeletes;
+    use HasApiTokens, HasFactory, Notifiable, SoftDeletes, HasUuid;
 
     protected $fillable = [
         'uuid',
