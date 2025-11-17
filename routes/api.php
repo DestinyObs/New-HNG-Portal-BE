02e9c20e062770e7695909c3618dd4f935046bd8
RichmanLoveday <lovedayrichman@yahoo.com>
2025-11-17 13:04:24 +0100

    fix(migration): removed job_listing_attributes migration

--- routes/api.php
+++ routes/api.php
@@ -9,4 +9,7 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
+Route::get('/test', function () {
+    dd("Reached her");
+});
 Route::post('/waitlist', [WaitlistController::class, 'store']);
