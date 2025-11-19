607a300eb3d95b922ce61b0d26df13420286ae2c
Usenmfon <usenmfonuko@gmail.com>
2025-11-19 13:10:21 +0100

    feat(interface): add interface to bind company repository logic

--- app/Repositories/Interfaces/CompanyRepositoryInterface.php
+++ app/Repositories/Interfaces/CompanyRepositoryInterface.php
@@ -5,4 +5,10 @@ namespace App\Repositories\Interfaces;
 interface CompanyRepositoryInterface
 {
     public function create(array $data);
+
+    public function show(string $uuid);
+
+    public function update(array $data, string $uuid);
+
+    public function updateLogo(mixed $file, string $uuid);
 }
