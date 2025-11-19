4aba3f7eccb84b77d645b2e2cd959d39dd078e40
dharmy05 <dharmy.dev05@gmail.com>
2025-11-19 10:37:44 +0100

    fix(employer route):  Fix the employer API route so the /company/{companyUuid}/jobs/{jobUuid}/applications   endpoint is properly recognized

--- routes/api/employer.php
+++ routes/api/employer.php
@@ -1,17 +1,17 @@
 <?php
 
-//? API routes for employer functionalities
-
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Employer\CompanyController;
 
-Route::prefix('api/employer')->group(function () {
-    Route::get('/test', function () {
-        dd("Employer route reached");
+Route::prefix('employer')->group(function () {
 
-        //company job application routes
-         Route::prefix('company/{companyUuid}/jobs/{jobUuid}')->group(function () {
+    Route::get('/test', function () {
+        return "Employer route reached";
+    });
+
+    // company job application routes
+    Route::prefix('company/{companyUuid}/jobs/{jobUuid}')->group(function () {
         Route::get('/applications', [CompanyController::class, 'getJobApplicants']);
     });
-    });
+
 });
