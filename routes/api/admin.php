d2eb715b463ed9e9cd6f1072419473d9595c8389
abdulsalamamtech <abdulsalamamtech@gmail.com>
2025-11-20 21:02:09 +0100

    feat (route) add location admin routes

--- routes/api/admin.php
+++ routes/api/admin.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Admin\LocationController;
 use Illuminate\Support\Facades\Route;
 
 //? API routes for admin functionalities
@@ -7,4 +8,8 @@ Route::prefix('api/admin')->group(function () {
     Route::get('/test', function () {
         dd("Admin route reached");
     });
+
+
+    // LOCATION ROUTES
+    Route::apiResource('locations', LocationController::class);
 });
