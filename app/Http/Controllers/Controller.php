d21553b832cf1baddfc13248c6a1eb376dfa6e36
Anita Rodney <anotarodney30@gmail.com>
2025-11-21 18:11:09 +0100

    fix():controller.php

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,8 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use App\Traits\ApiResponse as TraitsApiResponse;
-use App\Http\Controllers\Concerns\ApiResponse as ConcernsApiResponse;
+use App\Traits\ApiResponse;
 use App\Traits\UploadFile;
 
 abstract class Controller
