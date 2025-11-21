9a389ef2ed4ca7b1432f3c6ede95aa3ca4106227
techyJcrafts <joseph.adamu.it@gmail.com>
2025-11-21 15:10:29 +0100

    Revert "feat: comprehensive API routes and documentation update"

--- routes/api/employer.php
+++ routes/api/employer.php
@@ -3,10 +3,9 @@
 //? API routes for employer functionalities
 
 use App\Http\Controllers\Employer\CompanyController;
-use App\Http\Controllers\Employer\JobController;
 use Illuminate\Support\Facades\Route;
 
-Route::prefix('employer')->middleware('auth:sanctum')->group(function () {
+Route::prefix('api/employer')->group(function () {
     Route::get('/test', function () {
         dd("Employer route reached");
     });
@@ -15,16 +14,4 @@ Route::prefix('employer')->middleware('auth:sanctum')->group(function () {
     Route::get('company/{uuid}', [CompanyController::class, 'show']);
     Route::put('company/{uuid}', [CompanyController::class, 'update']);
     Route::put('company/{uuid}/logo', [CompanyController::class, 'updateLogo']);
-
-    // Nested job routes
-    Route::prefix('company/{uuid}/jobs')->group(function () {
-        Route::get('', [JobController::class, 'index']);
-        Route::post('', [JobController::class, 'store']);
-        Route::get('{job_id}', [JobController::class, 'show']);
-        Route::put('{job_id}', [JobController::class, 'update']);
-        Route::delete('{job_id}', [JobController::class, 'destroy']);
-        Route::put('{job_id}/restore', [JobController::class, 'restore']);
-        Route::put('{job_id}/publish', [JobController::class, 'publish']);
-        Route::put('{job_id}/unpublish', [JobController::class, 'unpublish']);
-    });
 });
--- routes/api/talent.php
+++ routes/api/talent.php
@@ -3,14 +3,10 @@
 
 //? API routes for talent functionalities
 
-use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
-Route::prefix('talent')->middleware('auth:sanctum')->group(function () {
+Route::prefix('api/talent')->group(function () {
     Route::get('/test', function () {
         dd("Talent route reached");
     });
-
-    Route::put('update-password', [UserController::class, 'updatePassword']);
-    Route::put('update-photo', [UserController::class, 'updatePhoto']);
 });
